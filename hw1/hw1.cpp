eca10399c987fce15b17d54f135c7cab2bb3a9a7
Alec Landow <alec.landow@cengage.com>
2021-05-15 23:49:58 -0400

    add formatting

--- hw1/hw1.cpp
+++ hw1/hw1.cpp
@@ -1,3 +1,6 @@
+// HW1
+// Author: Alec Landow
+
 // Convert this program to C++
 // change to C++ io
 // change to one line comments
@@ -13,30 +16,24 @@ using namespace std;
 
 const int N = 40;
 
-inline void sum(int& p, vector<int>& d) {
+inline void sum(int& p, vector<int>& data) {
+    p = 0;
 
-  int i;
-
-  p = 0;
-
-  int n = d.size();
-  for(i = 0; i < n; ++i)
-    p = p + d.at(i);
-
+    int n = data.size();
+    for(int i = 0; i < n; ++i)
+        p = p + data.at(i);
 }
 
 int main() {
-   int i;
-   int accum = 0;
-   vector<int> data;
+    int accum = 0;
+    vector<int> data;
 
-   for(i = 0; i < N; ++i)
-      data.push_back(i);
+    for(int i = 0; i < N; ++i)
+        data.push_back(i);
 
     sum(accum, data);
 
     cout << "sum is " << accum << endl;
 
     return 0;
-
 }
