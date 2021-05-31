ed9a3bc0d6eddc688fbb61d9f69d3d7ffc7f5e94
Alec Landow <alec.landow@cengage.com>
2021-05-30 22:43:12 -0400

    Add Graph.hpp header file

---
+++ hw2/Graph.hpp
@@ -0,0 +1,26 @@
+/*
+    The Graph class
+*/
+
+#include <vector>
+
+class Graph {
+    private:
+        int number_of_vertices;
+        int number_of_edges;
+        vector<vector<int>> adjacency_matrix;
+
+    public:
+        int get_number_of_vertices();
+        int get_number_of_edges();
+
+        bool are_adjacent(int node_x, int node_y);
+        vector<int> get_neighbors(int node_x);
+        bool add_edge(int node_x, int node_y);
+        bool delete_edge(int node_x, int node_y);
+
+        int get_node_value(int node_x);
+        void set_node_value(int node_x, int node_value);
+        int get_edge_value(int node_x, int node_y);
+        void set_edge_value(int node_x, int node_y, int edge_value);
+}
