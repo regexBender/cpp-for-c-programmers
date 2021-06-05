f61a8861ea6f906336a96f73ec3156d6e7443eec
Alec Landow <alec.landow@cengage.com>
2021-06-04 22:10:29 -0400

    Add back in progress code

--- hw2/PriorityQueue.hpp
+++ hw2/PriorityQueue.hpp
@@ -2,3 +2,43 @@
     Header file for PriorityQueue
     Author: Alec Landow
 */
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+class PriorityQueue {
+    private:
+        class Node {
+            private:
+                int index;
+                int priority;
+            public:
+                Node(int node_index, int priority) {
+                    this->index = node_index;
+                    this->priority = priority;
+                }
+        }
+        priority_queue<PriorityQueue::Node, vector<PriorityQueue::Node>, greater<PriorityQueue::Node::priority>> queue;
+
+        bool search_queue(
+            int node_index,
+            priority_queue<PriorityQueue::Node, vector<PriorityQueue::Node>, greater<PriorityQueue::Node::priority>> queue
+        );
+
+        bool change_priority_helper(
+            int node_index,
+            int new_priority,
+            priority_queue<PriorityQueue::Node, vector<PriorityQueue::Node>, greater<PriorityQueue::Node::priority>> queue
+        );
+
+    public:
+        bool change_priority(int node_index, int new_priority);
+        int min_priority();
+        bool contains(int node_index);
+        bool insert(int node_index, int priority);
+        int top();
+        int size();
+
+}
