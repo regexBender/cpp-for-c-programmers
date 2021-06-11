759249f723d29d50aace5949099ce1a6f294757c
Alec Landow <alec.landow@cengage.com>
2021-06-10 22:46:22 -0400

    Three tests pass

--- hw2/src/test/classes/GraphTester.cpp
+++ hw2/src/test/classes/GraphTester.cpp
@@ -1,8 +1,9 @@
 #include "../catch_amalgamated.hpp"
 #include "../../classes/Graph.hpp"
 
-TEST_CASE("Testing123", "[graph]") {
+TEST_CASE("Graph Parameters are Correct", "[graph]") {
     int number_of_nodes = 10;
+    int number_of_edges = 6;
     double edge_density = 0.5;
     double min_distance = 1.0;
     double max_distance = 9.0;
@@ -12,4 +13,6 @@ TEST_CASE("Testing123", "[graph]") {
     cout << graph;
 
     REQUIRE( number_of_nodes == graph.get_number_of_nodes() );
+    REQUIRE( number_of_edges == graph.get_number_of_edges() );
+    REQUIRE( graph.are_adjacent(0, 1) );
 }
